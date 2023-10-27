400abf574bebf17c42b481853bbc66381ddf81d2
LimaV2005 <129573390+LimaV2005@users.noreply.github.com>
2023-10-27 16:53:58 -0300

    Add files via upload

---
+++ src/app/repositories/SelecaoRepository.js
@@ -0,0 +1,124 @@
+import conexao, { consulta } from "../database/conexao.js"
+import bcrypt from "bcrypt"
+import  jwt  from "jsonwebtoken";
+///NOS REPOSITORIES, GUARDAR QUERYS E RESPOSTAS
+class SelecaoRepository {
+    
+
+    // consulta = (sql, valores="", mensagemReject) => {
+    //     return new Promise((resolve, reject) =>{
+    //             conexao.query(sql, valores,  (erro, result) =>{
+    //                 if(erro) return reject(mensagemReject)
+    //                 const row = JSON.parse(JSON.stringify(result))
+    //                 return resolve(row)
+    //             })
+    //         })
+
+    async create(email, senha) {
+        const hashedSenha = await bcrypt.hash(senha, 10);
+        const sql = 'INSERT INTO usuarios (email, senha) VALUES (?, ?)';
+        const values = [email, hashedSenha];
+        return await new Promise((resolve, reject) => {
+            conexao.query(sql, values, (erro, result) => {
+                if (erro) {
+                    reject('Não foi possível cadastrar');
+                } else {
+                    const row = JSON.parse(JSON.stringify(result));
+                    resolve(row);
+                }
+            });
+        });
+    }
+
+    findAll() {
+        const sql = 'SELECT * FROM usuarios;'
+        return consulta(sql, 'Não foi possivel localizar')
+    }
+
+    findById(id) {
+        const sql = "SELECT * FROM usuarios WHERE id=?;"
+        return consulta(sql, id, 'Não foi possivel localizar')
+    }
+
+
+    update(email, id) {
+        const sql = 'UPDATE usuarios SET ? WHERE id=?';
+
+        return new Promise((resolve, reject) => {
+            conexao.query(sql, [email, id], (erro, result) => {
+                if (erro) {
+                    reject('Não foi possível atualizar');
+                } else {
+                    const row = JSON.parse(JSON.stringify(result));
+                    resolve(row);
+                }
+            });
+        });
+    }
+
+    delete(id) {
+        const sql = 'DELETE FROM usuarios WHERE id=?';
+
+        return new Promise((resolve, reject) => {
+            conexao.query(sql, id, (erro, result) => {
+                if (erro) {
+                    reject('Não foi possível excluir');
+                } else {
+                    const row = JSON.parse(JSON.stringify(result));
+                    resolve(row);
+                }
+            });
+        });
+    }
+
+    // 
+    login(email, senha) {
+    const sql = 'SELECT * FROM usuarios WHERE email=?';
+
+    return new Promise((resolve, reject) => {
+        conexao.query(sql, [email], (erro, result) => {
+            if (erro) {
+                reject('Falha na consulta');
+            } else {
+                if (result) {
+                    const row = JSON.parse(JSON.stringify(result[0]));
+                    const senhaHash = row.senha;
+                    bcrypt.compare(senha, senhaHash, (err, result) => {
+                        if (err) {
+                            reject('Erro ao comparar senhas');
+                        } else if (result) {
+                            const token = jwt.sign({
+                                email: row.email,
+                                id: row.id
+                            }, 
+                            'segredo',
+                            {
+                                expiresIn: "1h"
+                            }
+                            )
+                            console.log('Logado com sucesso, token de sessão: ',token)
+                            resolve(row);
+                        } else {
+                            console.log('Comparação falhou: senha fornecida pelo usuario:', senha, 'senha no banco de dados:', senhaHash);
+                            reject('Senha incorreta');
+                        }
+                    });
+                } else {
+                    console.log('Nenhum usuário encontrado');
+                    reject('Usuário não encontrado');
+                }
+            }
+        });
+    });
+}
+
+
+
+
+
+
+
+
+}
+
+export default new SelecaoRepository
